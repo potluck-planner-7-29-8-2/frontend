5b56f020bfe242509bbf9ec4abd01125f55cde86
tommycoleman87 <tommiilee@gmail.com>
2019-07-29 14:33:00 -0400

    formatted the form field in signup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -13,39 +13,45 @@ const SignUp = props => {
     const name = event.target.name;
     const value = event.target.value;
     setUser({ ...user, [name]: value });
-    console.log(user);
   };
   return (
     <div>
       <form onSubmit={() => signUp(IS_SIGNING_UP, user)}>
-        <input
-          name="username"
-          type="text"
-          onChange={event => userInputHandler(event)}
-          value={user.username}
-          placeholder="Username"
-        />
-        <input
-          name="password"
-          type="password"
-          onChange={event => userInputHandler(event)}
-          value={user.password}
-          placeholder="Password"
-        />
-        <input
-          name="full_name"
-          type="text"
-          onChange={event => userInputHandler(event)}
-          value={user.full_name}
-          placeholder="Full Name"
-        />
-        <input
-          name="email"
-          type="email"
-          onChange={event => userInputHandler(event)}
-          value={user.email}
-          placeholder="E-Mail"
-        />
+        <fieldset>
+          <legend>Sign Up</legend>
+          Username:
+          <input
+            name="username"
+            type="text"
+            onChange={event => userInputHandler(event)}
+            value={user.username}
+            placeholder="Username"
+          />
+          Password:
+          <input
+            name="password"
+            type="password"
+            onChange={event => userInputHandler(event)}
+            value={user.password}
+            placeholder="Password"
+          />
+          Full Name:
+          <input
+            name="full_name"
+            type="text"
+            onChange={event => userInputHandler(event)}
+            value={user.full_name}
+            placeholder="Full Name"
+          />
+          E-Mail:
+          <input
+            name="email"
+            type="email"
+            onChange={event => userInputHandler(event)}
+            value={user.email}
+            placeholder="E-Mail"
+          />
+        </fieldset>
         <button>Sign Up</button>
       </form>
     </div>
