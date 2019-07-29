220586a335bbdbc1a38544f39a8da06fe2c0e089
May Ng <mngmay@gmail.com>
2019-07-29 12:06:36 -0700

    Changed Login submit button to say Login.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useStateValue } from "./hooks/useStateValue"; //must import this custom
 import { PrivateRoute } from "./utils/PrivateRoute"; //import to create a PrivateRoute - user can not get to that path unless logged in
 import TestComponent from "./components/TestComponent";
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 
 function App() {
   const [{ login }, dispatch] = useStateValue();
@@ -15,7 +16,7 @@ function App() {
       {/* <PrivateRoute path='/test' redirectURL='/home' component={SomeComponent}/>  
                 When using PrivateRoute, you need to give it the protected path, the path 
                 you want the user to be redirected to, and the component you want rendered*/}
-      <TestComponent />
+      {/* <TestComponent /> */}
       <SignUp />
       <Login />
     </div>
