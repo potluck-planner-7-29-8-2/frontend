83e729a33085e3b36dcbbed2e23d932d6747719a
tommycoleman87 <tommiilee@gmail.com>
2019-07-29 20:17:51 -0400

    working on functionality for event list

--- src/containers/EventList.js
+++ src/containers/EventList.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Event from '../components/Event';
-
+import AddEvent from '../components/AddEvent';
+import { getEvents } from '../actions/generalEventsActions';
+import { useStateValue } from '../hooks/useStateValue';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 /*{
     "event_name": "",
     "date": "",
@@ -11,15 +14,34 @@ import Event from '../components/Event';
     "state": ""
 }*/
 
+/*export const getEvents = (dispatch, id) => {
+    dispatch({type: GETTING_EVENTS})
+    axiosWithAuth()
+        .get(`/users/${id}/events`)
+        .then(res => {
+            dispatch({type: GOT_EVENTS, payload: res.data})
+        })
+        .catch(err => {
+            dispatch({type: EVENTS_ERROR, payload: err.response.data.message})
+        })
+}*/
+
 const EventList = () => {
-    const [events, setEvents] = useState([]);
+    const [eventList, setEventList] = useState([]);
+    const [{ events }, dispatch] = useStateValue();
+    const [user_id] = useLocalStorage("user_id");
+    console.log(eventList)
+    //console.log(getEvents(dispatch, user_id))
+
+    useEffect(() => {
+        setEventList(events.data);
+    },[events])
+
 
     return (
         <div>
             <h1>Events</h1>
-        {events.map(event => {
-           return <Event eventInfo={event} />
-        })}
+            <AddEvent />
         </div>
     )
 }
