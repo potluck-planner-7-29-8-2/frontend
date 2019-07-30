25537d7318c353188d614e7849b59e2e98f762a8
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-07-30 11:42:29 -0400

    Start styling, fix logout/login bug

--- src/index.js
+++ src/index.js
@@ -13,23 +13,41 @@ import * as global from './styles/global.css'
 
 const GlobalStyle = createGlobalStyle`
     ${reset} 
-    ${global} 
+    ${global}
+    h1 {
+        font-size: ${({theme}) => theme.hugeFont}
+    }
+    h2, h3 {
+        font-size: ${({theme}) => theme.largeFont}
+    }
+    h4, h5 {
+        font-size: ${({theme}) => theme.mediumFont}
+    }
+    body{
+        font-family: ${({theme}) => theme.loraFont}
+    }
 `
 
 const theme = {
-
+    tinyFont: '1.2rem',
+    smallFont: '1.8rem',
+    mediumFont: '2.4rem',
+    largeFont: '3.0rem',
+    hugeFont: '4.0rem',
+    poppinsFont: `"Poppins", sans-serif`,
+    loraFont: `"Lora", serif`
 }
 
 ReactDOM.render(
-    <>
-        <GlobalStyle />
         <ThemeProvider theme={theme}>
+        <>
+            <GlobalStyle />
             <Router>
                 <StateProvider initialState={initialState} reducer={mainReducer}> 
                     <App />
                 </StateProvider>
             </Router>
-        </ThemeProvider>
-    </>, 
+        </>
+        </ThemeProvider>, 
 document.getElementById('root'));
 
