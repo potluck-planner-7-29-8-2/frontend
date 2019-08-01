e9012d05a5599b7a2633adafc53a5c4c98894f6d
May Ng <mngmay@gmail.com>
2019-08-01 11:56:00 -0700

    rearranged icons for dish list

--- src/styled_components/SignUpPage/SignUpContainer.js
+++ src/styled_components/SignUpPage/SignUpContainer.js
@@ -1,41 +1,51 @@
-import styled from 'styled-components'
-import { LoginContainer } from '../index'
+import styled from "styled-components";
+import { LoginContainer } from "../index";
+import { device } from "../device";
 
 export const SignUpContainer = styled(LoginContainer)`
-    width: 900px;
-    flex-direction: row;
-    justify-content: space-between;
-    padding: 75px 0;
-    height: 500px; 
-    font-family: ${({theme}) => theme.poppinsFont};
-    font-weight: 400;
+  width: 900px;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 75px 0;
+  height: 500px;
+  font-family: ${({ theme }) => theme.poppinsFont};
+  font-weight: 400;
 
+  .sign_up_inner {
+    width: 49%;
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    padding-left: 34px;
 
-    .sign_up_inner{
-        width: 49%;
-        height: 400px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-        padding-left: 34px;
+    @media ${device.mobileL} {
+      width: 100%;
     }
+  }
 
-    .image{
-        padding-right: 15px;
-        img{
-            max-width: 100%;
-        }
-        a{
-            font-size: ${({theme}) => theme.smallFont};
-            color: #222;
-            line-height: 1.8;
-            text-decoration: underline;
-        }
+  .image {
+    padding-right: 15px;
+    img {
+      max-width: 100%;
     }
+    a {
+      font-size: ${({ theme }) => theme.smallFont};
+      color: #222;
+      line-height: 1.8;
+      text-decoration: underline;
+    }
+  }
 
-    .sign_up_error{
-        color: #dd2c00;
-        font-size: ${({theme}) => theme.tinyFont};
-    }
-`
+  .sign_up_error {
+    color: #dd2c00;
+    font-size: ${({ theme }) => theme.tinyFont};
+  }
+
+  @media ${device.mobileL} {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+`;
