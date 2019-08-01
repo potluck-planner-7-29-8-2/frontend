082eaea7c051c418325208c301740442c9ab5b3b
May Ng <mngmay@gmail.com>
2019-08-01 09:35:57 -0700

    adjusted login form mobile view width to eliminate horizontal scroll

--- src/styled_components/LoginPage/LoginContainer.js
+++ src/styled_components/LoginPage/LoginContainer.js
@@ -1,15 +1,20 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { device } from "../device";
 
-export const LoginContainer=styled.div`
-    margin: 100px auto;
-    width: 390px;
-    height: 650px;
-    background: #fff;
-    border-radius: 10px;
-    padding: 77px 55px 33px;
-    box-shadow: 0 5px 10px 0 rgba(0,0,0,.1);
-    display: flex;
-    flex-direction: column;
-    justify-contents: space-evenly;
-    align-items: center;
-`
+export const LoginContainer = styled.div`
+  margin: 100px auto;
+  width: 390px;
+  height: 650px;
+  background: #fff;
+  border-radius: 10px;
+  padding: 77px 55px 33px;
+  box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-contents: space-evenly;
+  align-items: center;
+
+  @media ${device.mobileL} {
+    width: 95%;
+  }
+`;
--- src/styled_components/LoginPage/SignUpDiv.js
+++ src/styled_components/LoginPage/SignUpDiv.js
@@ -1,26 +1,25 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const SignUpDiv = styled.div`
-    padding-top: 115px;
-    span {
-        font-family: ${({theme}) => theme.poppinsFont};
-        font-weight: 400;
-        font-size: ${({theme}) => theme.tinyFont};
-        color: #666;
-        line-height: 1.5;
-        padding-right: 5px;
-    }
-    a{
-        font-family: ${({theme}) => theme.poppinsFont};
-        font-weight: 400;
-        font-size: ${({theme}) => theme.tinyFont};
-        color: #333;
-        line-height: 1.5;
+  padding-top: 115px;
+  span {
+    font-family: ${({ theme }) => theme.poppinsFont};
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.tinyFont};
+    color: #666;
+    line-height: 1.5;
+    padding-right: 5px;
+  }
+  a {
+    font-family: ${({ theme }) => theme.poppinsFont};
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.tinyFont};
+    color: #333;
+    line-height: 1.5;
 
-        :hover{
-            text-decoration: underline;
-            color: ${({theme}) => theme.blue}
-        }
+    :hover {
+      text-decoration: underline;
+      color: ${({ theme }) => theme.blue};
     }
-
-`
+  }
+`;
